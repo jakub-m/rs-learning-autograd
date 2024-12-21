57404159fdc1772ca35a5665c015586d519e2058
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-12-21 11:29:49 +0100

    Lib

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod compute;
+pub mod core_syntax;
+pub mod float_calculator;
+pub mod float_syntax;
+pub mod forward_mode;
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-mod compute;
-mod core_syntax;
-mod float_calculator;
-mod float_syntax;
-mod forward_mode;
-mod test_derivate;
-fn main() {
-    println!("Hello, world!");
-}
