ef47d5b355cf5d8aa52f55a8244c0a0a237f28d4
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-12-21 17:07:31 +0100

    wip adjoin

--- tests/test_derivate.rs
+++ tests/test_derivate.rs
@@ -7,6 +7,8 @@ use rs_autograd::{
     },
 };
 
+use approx_eq::assert_approx_eq;
+
 mod utils;
 use utils::assert_functions_similar;
 
@@ -45,6 +47,7 @@ fn compare_simple_adjoin() {
     );
 }
 
+#[ignore]
 #[test]
 fn sin_cos() {
     let eb = new_eb();
@@ -84,12 +87,42 @@ fn test_hf() {
     let x1 = eb.new_variable("x1");
     let x2 = eb.new_variable("x2");
     let vm1 = x1;
-    let v0 = x1;
+    let v0 = x2;
     let v1 = vm1 * v0;
     let v2 = vm1.ln();
     let v3 = v0 + v1;
     let v4 = v3 - v2;
     let y = v4;
+
+    // Compute.
+    let x1 = x1.ident();
+    let x2 = x2.ident();
+    let v0 = v0.ident();
+    let v1 = v1.ident();
+    let v2 = v2.ident();
+    let v3 = v3.ident();
+    let v4 = v4.ident();
+    let y = y.ident();
+    let mut cg = ComputGraph::<f32, _, _>::new(eb, &FloatCalculator);
+    cg.set_variable(&x1, 3.0);
+    cg.set_variable(&x2, -4.0);
+
+    // Forward.
+    let eps = 0.01;
+    cg.forward(&y);
+    assert_approx_eq!(cg.primal(&y) as f64, -17.10, eps);
+
+    // Backward.
+    cg.backward(&y);
+    assert_approx_eq!(cg.adjoin(&y) as f64, 1.0, eps);
+    assert_approx_eq!(cg.adjoin(&v4) as f64, 1.0, eps);
+    assert_approx_eq!(cg.adjoin(&v3) as f64, 1.0, eps);
+    assert_approx_eq!(cg.adjoin(&v2) as f64, -1.0, eps);
+    assert_approx_eq!(cg.adjoin(&v1) as f64, 1.0, eps);
+    assert_approx_eq!(cg.adjoin(&x2) as f64, 4.0, eps);
+    assert_approx_eq!(cg.adjoin(&x1) as f64, -4.33, eps);
+
+    //cg.add_adjoin(&, adjoin);
 }
 
 fn new_eb() -> ExprBuilder<FloatOperAry1, FloatOperAry2> {
